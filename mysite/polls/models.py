93cd1a26c1e6520f26eeb3ea518a36158647dbc2
Rosendo Lopez Serrano <rosendo.lopez.contractor@bbva.com>
2022-09-24 09:49:58 -0500

    Primer commit proyecto polls django

---
+++ mysite/polls/models.py
@@ -0,0 +1,23 @@
+from pickle import TRUE
+from statistics import mode
+from django.db import models
+
+# Create your models here.
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    status = models.BooleanField(default=True)
+    order = models.IntegerField(null=True)
+
+    def __str__(self) :
+        return self.question_text
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+
+
+
---
+++ mysite/polls/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def index(request):
+    return HttpResponse("hola mundo")
+
+def hola_dos(request):
+    return HttpResponse("hola 2")    
+
