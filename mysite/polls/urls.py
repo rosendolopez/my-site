59c4cdfbbe95d53cde5bfebb0c4f683a7707b20c
Rosendo Lopez Serrano <rosendo.lopez.contractor@bbva.com>
2022-09-24 23:38:09 -0500

    Primer commit proyecto polls django (templates)

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -4,6 +4,14 @@ from . import views
 
 urlpatterns = [
     path('',views.index, name='index'),
-    path('hola2/',views.hola_dos, name='hola2'),
+    #path('hola2/',views.hola_dos, name='hola2'),
+    # ex: /polls/
+    #path('', views.index, name='index'),
+    # ex: /polls/5/
+    path('<int:question_id>/', views.detail, name='detail'),
+    # ex: /polls/5/results/
+    path('<int:question_id>/results/', views.results, name='results'),
+    # ex: /polls/5/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,11 +1,39 @@
+from multiprocessing import context
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question, Choice
+from django.template import loader
+from django.http import Http404
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("hola mundo")
+    questions = Question.objects.all()
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    #output = ', '.join([q.question_text for q in latest_question_list])
+    #return HttpResponse(output)
+    template = loader.get_template('polls/index.html')
+    context = {
+        'mensaje': "Lista de encuestas",
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
 
-def hola_dos(request):
-    return HttpResponse("hola 2")    
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    context = {
+        'mensaje': "Detalle de la encuestas",
+        'question': question,
+    }
+    return render(request, 'polls/detail.html', context)
+    #return HttpResponse("You're looking at question %s." % question_id)
 
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
